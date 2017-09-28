ebc2872a2111f01161927821eaf425b87b09f607
dhywty <clemwek@gmail.com>
2017-09-28 04:49:15 +0300

    [Chore] Add promise middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,15 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
+import promiseMiddleware from 'redux-promise-middleware';
 import reduxImmutableStateIvariant from 'redux-immutable-state-invariant';
 
 export default function configureStore(initialState) {
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(reduxImmutableStateIvariant())
+        applyMiddleware(
+            promiseMiddleware(),
+            reduxImmutableStateIvariant()
+        )
     );
 }
