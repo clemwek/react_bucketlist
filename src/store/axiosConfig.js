387b62e9592a9cd52043bdb0283e4f7abc5a6a24
dhywty <clemwek@gmail.com>
2017-09-28 09:04:45 +0300

    [CHORE] Add axios configuration

---
+++ src/store/axiosConfig.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import store from './configureStore';
+
+
+const instance = axios.create({
+    baseURL: 'http://localhost:5000',
+});
+
+
+// intercept requests and add authorization token
+instance.interceptors.request.use((config) => {
+    const token = store.getState().user.token;
+    if (token) {
+      config.headers.Authorization = `${token}`;
+    }
+    return config;
+});
+  
+export default instance;
