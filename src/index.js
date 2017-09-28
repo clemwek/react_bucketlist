f01ac9fca5e71af4f269bf1d58e5297b57fe6742
dhywty <clemwek@gmail.com>
2017-09-28 04:18:29 +0300

    [Chore] Connected the app with redux

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const store = configureStore();
+
 ReactDOM.render(
-    <Router history={browserHistory} routes={routes}></Router>,
+    <Provider>
+        <Router history={browserHistory} routes={routes}></Router>
+    </Provider>,
     document.getElementById('root')
 );
 registerServiceWorker();
