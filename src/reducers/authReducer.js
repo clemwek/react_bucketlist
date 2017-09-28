9641021350df79c13803bd2c2d7ae1792c9b02db
dhywty <clemwek@gmail.com>
2017-09-28 23:36:50 +0300

    [FEATURE] Add login reducer

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,10 +1,38 @@
 import * as types from '../actions/actionTypes';
+const initialState = {
+    authenticated: false,
+    loading: false,
+    login: true,
+    error: false,
+    message: '',
+    token: null
+}
 
-
-export default function authReducer(state=[], action) {
+export default function authReducer(state=initialState, action) {
     switch(action.type) {
-        case types.ONE:
-            return action.AUTH;
+        case 'LOGIN_PENDING':
+            return {...state, loading: true};
+        case 'LOGIN_REJECTED':
+            return {...state,
+                loading: false,
+                message: 'action.payload.data.message',
+                error: true
+            }
+        case 'LOGIN_FULFILLED':
+            return {...state,
+                loading: false,
+                token: action.payload.data.token,
+                message: action.payload.data.message,
+                authenticated: true,
+            }
+        case 'SHOW_LOGIN':
+            return {...state,
+                login: true
+            }
+        case 'SHOW_REGISTER':
+            return {...state,
+                login: false
+            }
 
         default:
             return state;
