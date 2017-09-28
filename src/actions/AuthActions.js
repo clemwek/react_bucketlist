3e6880390afc2a97e48abea2ca095385e194864a
dhywty <clemwek@gmail.com>
2017-09-28 23:35:18 +0300

    [FEATURE] Add login actions

--- src/actions/AuthActions.js
+++ src/actions/AuthActions.js
@@ -1,2 +1,48 @@
 import * as types from './actionTypes';
+import instance from '../store/axiosConfig';
 
+export function login(data) {
+    return {
+        type: types.LOGIN,
+        payload: instance.post('/auth/login', data),
+    }
+}
+
+export function showLogin () {
+    return {
+        type: types.SHOW_LOGIN,
+    }
+}
+
+export function showRegister () {
+    return {
+        type: types.SHOW_REGISTER,
+    }
+}
+
+// export function logout () {
+//     return {
+//         type: types.LOGOUT,
+//         payload: instance.post('/auth/logout'),
+//     }
+// }
+
+// export function loginSuccess(data) {
+//     return {
+//         type: types.LOGIN_SUCCESS,
+//         data
+//     }
+// }
+
+// export function login(data) {
+//     return function (dispatch) {
+//         return instance
+//         .post('/auth/login', data)
+//         .then(resp => {
+//             dispatch(loginSuccess(resp.data));
+//         })
+//         .catch(error => {
+//             console.log(error.response.data)
+//         })
+//     }
+// }
---
+++ src/actions/actionTypes.js
@@ -0,0 +1,4 @@
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN = 'LOGIN';
+export const SHOW_LOGIN = 'SHOW_LOGIN';
+export const SHOW_REGISTER = 'SHOW_REGISTER';
