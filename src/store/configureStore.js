46270792dc014616a73dc07946ea8f9e6aad6da9
dhywty <clemwek@gmail.com>
2017-09-28 09:04:13 +0300

    [FIX] Add create store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,15 +1,17 @@
 import {createStore, applyMiddleware} from 'redux';
-import rootReducer from '../reducers';
 import promiseMiddleware from 'redux-promise-middleware';
 import reduxImmutableStateIvariant from 'redux-immutable-state-invariant';
+import { logger } from 'redux-logger'
 
-export default function configureStore(initialState) {
-    return createStore(
-        rootReducer,
-        initialState,
-        applyMiddleware(
-            promiseMiddleware(),
-            reduxImmutableStateIvariant()
-        )
-    );
-}
+import rootReducer from '../reducers';
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(
+        promiseMiddleware(),
+        reduxImmutableStateIvariant(),
+        logger
+    )
+);
+
+export default store;
