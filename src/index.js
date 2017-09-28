183f43ef69673963dcfce100cf33d165ecadc4b9
dhywty <clemwek@gmail.com>
2017-09-28 09:03:44 +0300

    [FIX] Moved create store store config

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
-import configureStore from './store/configureStore';
+import store from './store/configureStore';
 import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = configureStore();
 
 ReactDOM.render(
     <Provider store={store}>
