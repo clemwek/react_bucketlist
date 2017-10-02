b0b2839c973601daab454815273a316a7d4c29f9
dhywty <clemwek@gmail.com>
2017-10-02 11:25:21 +0300

    [FIX] Fixed adding items

--- src/reducers/bucketlistReducer.js
+++ src/reducers/bucketlistReducer.js
@@ -18,9 +18,9 @@ export default function bucketlistReducer(state=[], action) {
                 Object.assign({}, action.bucketlist)
             ]
         case types.ADD_ITEM_SUCCESS:
-            return state.map(bucket => bucket.id === action.bucketlist.bucket_id ?
-                {...bucket, items: action.bucketlist} : bucket
-            )
+            return [...state['items'], 
+            Object.assign({}, action.bucketlist)
+        ]
         case types.DELETE_BUCKETLIST_SUCCESS:
             return state.filter(bucketlist => bucketlist.id !== action.bucketlist.id)
         case types.DELETE_BUCKETLIST_ITEM_SUCCESS:
